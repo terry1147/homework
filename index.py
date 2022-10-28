31d47ab3a35c50d6e111a4634e8a6ff66fa1541c
terry1147 <s1101733@gm.pu.edu.tw>
2022-10-28 21:15:01 +0800

    version 0: html5

--- index.py
+++ index.py
@@ -7,8 +7,6 @@ app = Flask(__name__)
 def index():
     homepage = "<h1>田韻琪個人網頁</h1>"
     homepage += "<a href=/mis>MIS</a><br>"
-    homepage += "<a href=/today>顯示日期時間</a><br>"
-    homepage += "<a href=/welcome?nick=terry>傳送使用者名稱</a><br>"
     homepage += "<a href=/code>興趣何倫碼</a><br>"
     homepage += "<a href=/work>工作</a><br>"
     homepage += "<a href=/my>自傳履歷</a><br>"
@@ -18,16 +16,6 @@ def index():
 @app.route("/mis")
 def course():
     return "<h1>資訊管理導論</h1>"
-
-@app.route("/today")
-def today():
-    now = datetime.now()
-    return render_template("today.html", datetime = str(now))
-
-@app.route("/welcome", methods=["GET", "POST"])
-def welcome():
-    user = request.values.get("nick")
-    return render_template("welcome.html", name=user)
 
 @app.route("/code")
 def code():
